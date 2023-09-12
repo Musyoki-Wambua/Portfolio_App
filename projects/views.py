d2451ad4bf560b4e664a27d31a875f0dae215323
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-09-12 07:52:20 +0300

    Added routes

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from projects.models import Project
 
-def project_index(request, pk):
+def project_index(request):
     projects = Project.objects.all()
     context = {
         "projects": projects
