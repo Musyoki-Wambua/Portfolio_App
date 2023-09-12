2aef7c1937bb32338107bc1d99d263a6b5e6c88b
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-09-12 06:26:05 +0300

    Created Project Model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    decsription = models.Textfield()
+    technology = models.CharField(max_length=20)
